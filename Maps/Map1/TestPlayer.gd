6fca572da2f2068d633c2f95fa42740748528a97
Li Zi Ying <liziying@Lis-MacBook-Air-2.local>
2019-06-20 16:03:58 +0800

    Add final design of Map1 and include one way collision platforms

---
+++ Maps/Map1/TestPlayer.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+const SPEED = 60
+const GRAVITY = 10
+const JUMP_POWER = -250
+const FLOOR = Vector2(0, -1)
+
+var velo = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velo.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velo.x = -SPEED
+	else:
+		velo.x = 0
+		
+	if Input.is_action_pressed("ui_up"):
+		velo.y = JUMP_POWER
+		
+	velo.y += GRAVITY
+	
+	velo = move_and_slide(velo)
