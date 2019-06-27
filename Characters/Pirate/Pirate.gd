b517af0899c783be41026754a417f033ca84ccb6
Aaron Choo <choozy.aaron@gmail.com>
2019-06-27 23:06:00 +0800

    Bugfix Pirate animations and code

--- Characters/Pirate/Pirate.gd
+++ Characters/Pirate/Pirate.gd
@@ -39,17 +39,16 @@ func _ready():
 	
 func _physics_process(delta):
 
-		direction_input()				# horizontal mvmt
-		jump_input()					# jump
-		acceleration_curve()			# simluate acceleration when moving
-		attack_input()
-		flip_sprite(x_dir)				# flips sprite when turning direction
-		play_animation(x_dir)
-		
-		print(is_on_floor())
-		$Camera2D.current = true
-		velocity.y += gravity * delta 	# gravity
-		velocity = move_and_slide(velocity, FLOOR)	# godot's physics
+	direction_input()				# horizontal mvmt
+	jump_input()					# jump
+	acceleration_curve()			# simluate acceleration when moving
+	attack_input()
+	flip_sprite(x_dir)				# flips sprite when turning direction
+	play_animation(x_dir)
+	
+	$Camera2D.current = true
+	velocity.y += gravity * delta 	# gravity
+	velocity = move_and_slide(velocity, FLOOR)	# godot's physics
 	
 	
 func direction_input():
@@ -82,7 +81,7 @@ func jump_input():
 	if !is_dead:	
 		if Input.is_action_just_pressed("ui_up"):
 			if !is_attacking:
-				if !is_on_floor():
+				if is_on_floor():
 					velocity.y = max_jump_velocity
 		
 		# variable jump height
