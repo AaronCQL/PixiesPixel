6b3435771175679c49ec1ed020621ca47a6f88c5
Li Zi Ying <ziying338@gmail.com>
2019-07-24 20:05:10 +0800

    Able to enter and exit Settings from Main Menu

--- Settings/Settings.gd
+++ Settings/Settings.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 var audio_player
 
 func _ready():
+	Network.connect("on_exit_button_pressed", self, "_show_main_menu")
 	audio_player = get_node("AudioStreamPlayer2D")
 
 func _on_CheckButton_toggled(button_pressed):
@@ -11,7 +12,9 @@ func _on_CheckButton_toggled(button_pressed):
 	else:
 		audio_player.stop()
 
-
 func _on_VolumeSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(0, value)
-	
+
+func _on_BackButton_pressed():
+	Network.exit_to_main_menu()
+	$Panel.hide()
