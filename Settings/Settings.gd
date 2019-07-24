b49e4d4e19d3d380af2635830c2d029fa62d5ec8
Aaron Choo <choozy.aaron@gmail.com>
2019-07-24 21:55:37 +0800

    Separate Music from Sound Effects

--- Settings/Settings.gd
+++ Settings/Settings.gd
@@ -7,15 +7,24 @@ func _ready():
 	Network.connect("on_exit_button_pressed", self, "_show_main_menu")
 	audio_player = get_node("AudioStreamPlayer")
 
-func _on_CheckButton_toggled(button_pressed):
+func _on_MusicToggle_toggled(button_pressed):
 	if button_pressed == true:
-		audio_player.play()
+		AudioServer.set_bus_mute(1, false)
 	else:
-		audio_player.stop()
+		AudioServer.set_bus_mute(1, true)
 
-func _on_VolumeSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(0, value)
+func _on_MusicSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(1, value)
 
+func _on_SoundEffectsToggle_toggled(button_pressed):
+	if button_pressed == true:
+		AudioServer.set_bus_mute(2, false)
+	else:
+		AudioServer.set_bus_mute(2, true)
+
+func _on_SoundEffectsSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(2, value)
+			
 func _on_BackButton_pressed():
 	get_node("/root/MainMenu/NetworkPanel").show()
 	$Panel.hide()
